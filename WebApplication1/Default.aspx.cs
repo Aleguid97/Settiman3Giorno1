200d1a27a3dd95c87888f63f7d65041c40cc40ca
Aleguid97 <aleguid97@gmail.com>
2024-02-12 15:08:11 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = ConfigurationManager.AppSettings["NomeCandidato"];
+        }
+
+
+
+    }
+}
